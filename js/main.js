56c84c0da4098b8d15808f4babe3917bfdfb9951
Imagineskies <imagineskies@protonmail.com>
2019-06-29 10:20:08 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ $(".sliding-link").click(function(e) {
     console.log(aid)
     $('html,body').animate({scrollTop: $(aid).offset().top},1500);
 });
+
+window.onload = function() {
+  var main = document.getElementById('main');
+  var loadingScreen = document.getElementById('loadingScreen');
+  main.classList.remove('hidden');
+  loadingScreen.classList.remove('shown', 'overhidden');
+  loadingScreen.classList.add('hidden');
+}
