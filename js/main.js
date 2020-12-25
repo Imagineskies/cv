50c6f706ac83913151e5fb4ff8b9db1ae61d6689
Rayna Weeden <imagineskies@protonmail.com>
2020-12-25 00:08:43 -0500

    Update to Work Experience

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+'use strict';
+// Variable
+
+
+// Declarations
+
+
+// Constants
+
+
+
+/*
+____________________________________________________________________________________
+Functions
+____________________________________________________________________________________
+*/
+
+
+function setHeaderHeight() {
+  var height = screen.height;
+  document.documentElement.style.setProperty("--header-height", height + 'px');
+  console.log(height);
+}
+
+
+
+
+/*
+____________________________________________________________________________________
+Event Listeners
+____________________________________________________________________________________
+*/
+
 $(".sliding-link").click(function(e) {
     e.preventDefault();
     var aid = $(this).attr("href");
